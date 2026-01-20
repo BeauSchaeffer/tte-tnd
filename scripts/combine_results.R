6b8baa768a2cab2079df103ccf9acc937f4fc105
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-01-20 15:44:44 -0500

    split comb res and res table

--- scripts/combine_results.R
+++ scripts/combine_results.R
@@ -1,5 +1,5 @@
 ##----- Kaiser Causal TTE-TND
-##----- Combine Results
+##----- Combine Results and Draft Figures
 
 
 # Packages ----------------------------------------------------------------
@@ -66,7 +66,7 @@ res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results/
     ) |>
     arrange(sim, time_end)
   
-  saveRDS(pci.itt.boot.long, file.path(res_path, "pci.itt.boot.long.rds")) # 2025-01-16
+  # saveRDS(pci.itt.boot.long, file.path(res_path, "pci.itt.boot.long.rds")) # 2025-01-16
   
   
   
@@ -227,7 +227,7 @@ plot.risk.with.boot.ci <- function(risks.and.cis,
 }
 
 
-# STD ITT -----------------------------------------------------------------
+# STD ITT draft plot ------------------------------------------------------
 
 
 std.itt.risks.ci <- pooled.boot.ci(point.est = std.itt.risk.pointest, boot.long = std.itt.boot.long)
@@ -238,7 +238,7 @@ plot.risk.with.boot.ci(std.itt.risks.ci, title.sub  = "Standard TTE (ITT)")
 # dev.off()
 
 
-# EQC ITT -----------------------------------------------------------------
+# EQC ITT draft plot ------------------------------------------------------
 
 
 eqc.itt.HRs.ci <- eqc.cox.boot.ci(eqc.itt.cox.pointest, eqc.itt.cox.boot.long)
@@ -252,7 +252,7 @@ plot.risk.with.boot.ci(eqc.itt.risks.ci, title.sub  = "Equi-confounding (ITT)")
 # dev.off()
 
 
-# PCI ITT -----------------------------------------------------------------
+# PCI ITT draft plot ------------------------------------------------------
 
 pci.itt.HRs.ci <- pci.cox.boot.ci(pci.itt.cox.pointest, pci.itt.cox.boot.long)
 # saveRDS(pci.itt.HRs.ci, paste0(res_path, "pci.itt.HRs.ci.rds")) # 2026-01-12
@@ -263,6 +263,10 @@ pci.itt.risks.ci <- pooled.boot.ci(point.est = pci.itt.risk.pointest, boot.long 
 # png("figures_draft/pci.itt.risks.ci.plot.png", width = 2400, height=1800, res=300)
 plot.risk.with.boot.ci(pci.itt.risks.ci, title.sub  = "Proximal inference (ITT)")
 # dev.off()
+
+
+
+
 
 
 
