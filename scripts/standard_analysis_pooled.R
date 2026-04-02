de641efca6ebb1ed6aa5a650172a794864446562
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-04-02 16:27:48 -0400

    input output paths weekmatch

--- scripts/standard_analysis_pooled.R
+++ scripts/standard_analysis_pooled.R
@@ -16,10 +16,11 @@ library(geepack)
 # Data --------------------------------------------------------------------
 
 
-data_Y2 <- read_rds("/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/data_Y2.rds")
+# data_Y2 <- read_rds("/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/data_Y2.rds")
+data_Y2 <- read_rds("/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/data_weekmatch/data_Y2_weekmatch.rds")
 dat <- data_Y2
 
-res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results/"
+res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_weekmatch/"
 
 # Downsample --------------------------------------------------------------
 
