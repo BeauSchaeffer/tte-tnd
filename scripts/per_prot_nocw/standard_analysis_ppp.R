930d622d41d31b1eea2880312b59326699e8ce2a
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-06-30 16:59:57 -0400

    move pseudo pp to sep script

--- scripts/per_prot_nocw/standard_analysis_ppp.R
+++ scripts/per_prot_nocw/standard_analysis_ppp.R
@@ -1,6 +1,7 @@
 ##----- Beau Schaeffer
 ##----- Kaiser Causal TTE-TND
 ##----- Standard Analysis
+##----- Per-protocol, no censoring weights
 
 # Packages ----------------------------------------------------------------
 
@@ -16,19 +17,18 @@ library(data.table)
 # Data --------------------------------------------------------------------
 
 
-# data_Y2 <- read_rds("/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/data_Y2.rds")
 data_Y2 <- read_rds("/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/data_weekmatch/data_Y2_weekmatch.rds")
 
-res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_weekmatch/"
+res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_weekmatch_ppp/"
 
 
 # Cox Model ---------------------------------------------------------------
 
 
-# Intention to Treat
+# Per-Protocol
 
-std_Y2_cox_itt <- coxph(
-  Surv(Y2_itt_t_trunc, Y2_itt_trunc) ~ treatment + 
+std_Y2_cox_ppp <- coxph(
+  Surv(Y2_pp_t_trunc, Y2_pp_trunc) ~ treatment + 
     sex_admin + age_years + bmi + race + charlson_cat_fac +
     # other
     ndi + prior_inf + tests_count + service_region + last_vax_infect_weeks +
@@ -37,9 +37,17 @@ std_Y2_cox_itt <- coxph(
     cluster(subclass),
   data = data_Y2)
 
-summary(std_Y2_cox_itt)
+summary(std_Y2_cox_ppp)
 
-std_Y2_cox_itt_tidy <- tidycmprsk::tidy(std_Y2_cox_itt, conf.int = TRUE, exponentiate = TRUE)
-std_Y2_cox_itt_tidy
-saveRDS(std_Y2_cox_itt_tidy, paste0(res_path,"std.itt.cox.pointest.rds")) # 2026-03-03
+std_Y2_cox_ppp_tidy <- tidycmprsk::tidy(std_Y2_cox_ppp, conf.int = TRUE, exponentiate = TRUE)
+std_Y2_cox_ppp_tidy
+saveRDS(std_Y2_cox_ppp_tidy, paste0(res_path,"std.ppp.cox.pointest.rds")) # 2026-06-30
+
+
+
+
+
+
+
+
 
