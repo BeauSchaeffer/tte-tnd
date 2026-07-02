2dafdf2f11e03c54bdd81886dff28b8ea76920e5
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-07-02 14:46:53 -0400

    add pp results tables

---
+++ scripts/per_prot_nocw/res_table_pp.R
@@ -0,0 +1,219 @@
+##----- Kaiser Causal TTE-TND
+##----- Results Table
+##----- Per-protocol, no censoring weights
+
+
+# Packages ----------------------------------------------------------------
+
+
+library(tidyverse)
+library(kableExtra)
+
+
+# Data --------------------------------------------------------------------
+
+
+res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_weekmatch_pp/"
+
+# STD Cox
+std.pp.cox.pointest <- readRDS(paste0(res_path,"std.pp.cox.pointest.rds"))
+
+# STD Pooled
+std.pp.risks.ci <- readRDS(paste0(res_path, "std.pp.risks.ci.rds"))
+
+# TND
+tnd.pp.pointest <- readRDS(paste0(res_path,"tnd.pp.pointest.rds"))
+
+# EQC Cox
+eqc.pp.HRs.ci <- readRDS(paste0(res_path, "eqc.pp.HRs.ci.rds"))
+
+# EQC Pooled
+eqc.pp.risks.ci <- readRDS(paste0(res_path, "eqc.pp.risks.ci.rds"))
+
+# PCI Cox
+pci.pp.HRs.ci <- readRDS(paste0(res_path, "pci.pp.HRs.ci.rds"))
+
+# PCI Pooled
+pci.pp.risks.ci <- readRDS(paste0(res_path, "pci.pp.risks.ci.rds"))
+
+
+# Cox estimates table -----------------------------------------------------
+
+label_exposure <- function(term) {
+  case_when(
+    term == "treatment" ~ "Treatment",
+    term == "flu_vax"   ~ "Flu vax",
+    TRUE                ~ term
+  )
+}
+
+std_tbl <- std.pp.cox.pointest |> 
+  filter(term %in% c("treatment", "flu_vax")) |> 
+  transmute(
+    approach  = "STD",
+    term,
+    exposure  = label_exposure(term),
+    effect    = estimate,
+    ci_low    = conf.low,
+    ci_high   = conf.high
+  ) |> 
+  select(-term)
+
+tnd_tbl <- tnd.pp.pointest |> 
+  filter(term %in% c("treatment", "flu_vax")) |> 
+  transmute(
+    approach = "TND",
+    term,
+    exposure = label_exposure(term),
+    effect   = OR,
+    ci_low   = LCL,
+    ci_high  = UCL
+  ) |> 
+  select(-term)
+
+
+eqc_tbl <- eqc.pp.HRs.ci %>%
+  pivot_longer(everything(), names_to = "name", values_to = "value") %>%
+  mutate(
+    exposure = case_when(
+      grepl("^treatHR",  name) ~ "Treatment",
+      grepl("^fluvaxHR", name) ~ "Flu vax",
+      TRUE ~ NA_character_
+    ),
+    metric = case_when(
+      grepl("_lo$", name) ~ "ci_low",
+      grepl("_hi$", name) ~ "ci_high",
+      name %in% c("treatHR", "fluvaxHR") ~ "effect",
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(exposure), !is.na(metric)) |> 
+  select(exposure, metric, value) |> 
+  pivot_wider(names_from = metric, values_from = value) |> 
+  mutate(approach = "EQC") |> 
+  select(approach, exposure, effect, ci_low, ci_high)
+
+pci_tbl <- pci.pp.HRs.ci |> 
+  transmute(
+    approach = "PCI",
+    exposure = "Treatment",
+    effect   = treatHR,
+    ci_low   = treatHR_lo,
+    ci_high  = treatHR_hi
+  )
+
+draft_table <- bind_rows(std_tbl, tnd_tbl, eqc_tbl, pci_tbl) |> 
+  mutate(ve = (1-effect)*100,
+         ve_low = (1-ci_high)*100,
+         ve_high = (1-ci_low)*100) |> 
+  mutate(
+    effect.ci = sprintf("%.2f (%.2f, %.2f)", effect, ci_low, ci_high),
+    ve.ci = sprintf("%.2f (%.2f, %.2f)", ve, ve_low, ve_high)
+  ) |> 
+  select(approach, exposure, effect.ci, ve.ci)
+
+draft_table
+
+wide_table <- draft_table |>
+  
+  pivot_longer(
+    cols = c(effect.ci, ve.ci),
+    names_to = "metric",
+    values_to = "value"
+  ) |>
+  
+  mutate(
+    metric = recode(metric,
+                    "effect.ci" = "HR",
+                    "ve.ci"     = "VE (%)"
+    ),
+    metric = factor(metric, levels = c("HR", "VE (%)"))
+  ) |>
+  
+  pivot_wider(
+    names_from  = c(exposure, metric),
+    values_from = value,
+    names_sep   = "_"
+  ) |>
+  
+  select(
+    approach,
+    Treatment_HR, `Treatment_VE (%)`,
+    `Flu vax_HR`, `Flu vax_VE (%)`
+  )
+
+wide_table
+
+
+wide_table |>
+  kbl(
+    format = "latex",
+    caption   = "Cox results (pp no cw)",
+    label     = "cox_res_pp",
+    booktabs = TRUE,
+    align = "lcccc",
+    col.names = c("Approach", "HR", "VE (%)", "HR", "VE (%)")
+  ) |>
+  add_header_above(c(" " = 1, "Treatment" = 2, "Flu vaccine" = 2)) |>
+  kable_styling(full_width = FALSE)
+
+
+# Pooled estimates table --------------------------------------------------
+
+# horizons <- c(1, 8, 24, 40, 52)
+horizons <- seq(1,52,1)
+
+make_pooled_risk_table <- function(df, approach, horizons) {
+  df |> 
+    filter(time_end %in% horizons) %>%
+    mutate(
+      `Risk Ratio (95\\% CI)` = sprintf("%.2f (%.2f, %.2f)", rr, rr_lo, rr_hi),
+      `Risk Difference (95\\% CI)` = sprintf("%.4f (%.4f, %.4f)", rd, rd_lo, rd_hi),
+    ) |> 
+    transmute(
+      `Time (weeks)` = time_end,
+      `Risk Ratio (95\\% CI)`,
+      `Risk Difference (95\\% CI)`
+    )
+}
+
+std_risk_tbl <- make_pooled_risk_table(std.itt.risks.ci, "STD", horizons)
+eqc_risk_tbl <- make_pooled_risk_table(eqc.itt.risks.ci, "EQC", horizons)
+pci_risk_tbl <- make_pooled_risk_table(pci.itt.risks.ci, "PCI", horizons)
+
+knitr::kable(
+  std_risk_tbl,
+  format = "latex",
+  booktabs = TRUE,
+  caption = "Pooled logistic results by week (STD)",
+  label = "std_rr_rd",
+  escape = F
+)
+
+knitr::kable(
+  eqc_risk_tbl,
+  format = "latex",
+  booktabs = TRUE,
+  caption = "Pooled logistic results by week (EQC)",
+  label = "eqc_rr_rd",
+  escape = F
+)
+
+knitr::kable(
+  pci_risk_tbl,
+  format = "latex",
+  booktabs = TRUE,
+  caption = "Pooled logistic results by week (PCI)",
+  label = "pci_rr_rd",
+  escape = F
+)
+
+
+
+
+
+
+
+
+
+
