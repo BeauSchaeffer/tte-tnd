14f19308d45e8379d9b7cb3bef1d2cead2ae5538
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-08-06 15:08:56 -0400

    v4 results

--- scripts/pp_scripts/combine_results_pp.R
+++ scripts/pp_scripts/combine_results_pp.R
@@ -1,7 +1,7 @@
 ##----- Kaiser Causal TTE-TND
 ##----- Combine Results and Draft Figures
 ##----- Per-protocol, no censoring weights
-
+##----- Last updated 2026-08-06
 
 # Packages ----------------------------------------------------------------
 
@@ -12,8 +12,8 @@ library(tidyverse)
 # Data --------------------------------------------------------------------
 
 
-res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_pp.3/"
-plot_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_pp.3/"
+res_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_pp.4/"
+plot_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_pp.4/"
 
 # STD Cox
 
@@ -47,7 +47,7 @@ plot_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_p
 # PCI Pooled
   pci.pp.risk.pointest <- readRDS(paste0(res_path, "pci.pp.risk.pointest.rds"))
 
-  pci_rep_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_pp.3/pci_boot_reps"
+  pci_rep_path <- "/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/results_pp.4/pci_boot_reps"
   
   pci_rep_files <- list.files(
     pci_rep_path,
