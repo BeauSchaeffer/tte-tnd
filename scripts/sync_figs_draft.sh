d34df7bc3a5d5102422f185e422d0f42d51b2c16
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-08-06 15:09:24 -0400

    v4 res

--- scripts/sync_figs_draft.sh
+++ scripts/sync_figs_draft.sh
@@ -1,7 +1,7 @@
 rsync -avh --progress \
-  bschaeffer@login.rc.fas.harvard.edu:/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_itt.3/ \
-  /Users/beauschaeffer/Desktop/Research/tte-tnd/plots_itt.3/
+  bschaeffer@login.rc.fas.harvard.edu:/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_itt.4/ \
+  /Users/beauschaeffer/Desktop/Research/tte-tnd/plots_itt.4/
 
 rsync -avh --progress \
-  bschaeffer@login.rc.fas.harvard.edu:/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_pp.3/ \
-  /Users/beauschaeffer/Desktop/Research/tte-tnd/plots_pp.3/
+  bschaeffer@login.rc.fas.harvard.edu:/n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/kaiser/plots_pp.4/ \
+  /Users/beauschaeffer/Desktop/Research/tte-tnd/plots_pp.4/
