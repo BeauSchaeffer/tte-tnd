63263a9350ee1ee31f5a66f3092a9b2dee24cb90
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-08-12 14:16:15 -0400

    no interaction check against cox, both stages

--- scripts/pp_scripts/proximal_inference_pooled_pp.R
+++ scripts/pp_scripts/proximal_inference_pooled_pp.R
@@ -108,20 +108,34 @@ prox_pooled_pp_s2 <- speedglm(Y_pos ~ ns(time_end, knots = c(10,20,30,40,50))*tr
 saveRDS(prox_pooled_pp_s2, paste0(res_path,"prox_pooled_pp_s2.rds"))
 
 # # sanity check against cox model
-# # stage 2 with no tx interaction
+# # stage 1 and 2 with no tx interaction terms
+# prox_pooled_pp_s1_noint <- speedglm(Y_neg ~ ns(time_end, knots = c(10,20,30,40,50)) + treatment +
+#                                                                                  # demographic
+#                                                                                  sex_admin + age_years + bmi + race + charlson_cat_fac +
+#                                                                                  # other
+#                                                                                  ndi + prior_inf + tests_count + service_region + last_vax_infect_weeks + 
+#                                                                                  # NEC
+#                                                                                  flu_vax,
+#                               data=dat.long.pp,
+#                               family=binomial(),
+#                               sparse = FALSE)
+# saveRDS(prox_pooled_pp_s1_noint, paste0(res_path,"prox_pooled_pp_s1_noint.rds"))
+# 
+# dat.long.pp$p_pp_noint <- predict(prox_pooled_pp_s1_noint, newdata = dat.long.pp)
+# 
 # prox_pooled_pp_s2_noint <- speedglm(Y_pos ~ ns(time_end, knots = c(10,20,30,40,50)) + treatment +
 #                                 # demographic
 #                                 sex_admin + age_years + bmi + race + charlson_cat_fac +
 #                                 # other
 #                                 ndi + prior_inf + tests_count + service_region + last_vax_infect_weeks +
 #                                 # predictions from stage 1
-#                                 p_pp,
+#                                 p_pp_noint,
 #                               # no NEC
 #                               data=dat.long.pp,
 #                               family=binomial(),
 #                               sparse = FALSE)
 # saveRDS(prox_pooled_pp_s2_noint, paste0(res_path,"prox_pooled_pp_s2_noint.rds"))
-# exp(prox_pooled_pp_s2_noint$coefficients["treatment"])
+# exp(prox_pooled_pp_s2_noint$coefficients)
 
 prox_pooled_pp_obs <- speedglm(Y_pos ~ ns(time_end, knots = c(10,20,30,40,50))*treatment +
                                   # demographic
